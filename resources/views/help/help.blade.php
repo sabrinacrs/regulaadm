807071bd0138f623c2d03914788335c908cee7d4
Sabrinas <sabrinacrslv@gmail.com>
2017-12-01 15:02:01 -0200

    Manual do usuário integrado - correção

--- resources/views/help/help.blade.php
+++ resources/views/help/help.blade.php
@@ -7,7 +7,7 @@
 <meta name="HandheldFriendly" content="True">
 <meta name="MobileOptimized" content="320">
 <meta name="viewport" content="initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<title>COTTONAPP - Manual do Usuário</title>
+<title>CottonApp - Manual do Usuário</title>
 <link rel="alternate" type="application/rss+xml" title="frittt.com" href="feed/index.html">
 <link href="http://fonts.googleapis.com/css?family=Raleway:700,300" rel="stylesheet"
         type="text/css">
